a3bc046914cf378cc7e38d0a8f2ccb60ef370060
sumbalilyas <sumbalch99@gmail.com>
2019-06-14 23:49:17 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+
+import Login from './src/pages/Login';
+import Signup from './src/pages/Signup';
+import Navigation from './src/pages/Navigation';
+import Routes from './src/Routes';
+
+export default class App extends Component {
+  render() {
+    return (
+     
+
+      // <View style={styles.container}>
+      //   <StatusBar
+      //      backgroundColor="#1c313a"
+      //      barStyle="light-content"
+      //    />
+  // <Routes/>
+<Routes/>
+  
+
+   
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#f5f5f5',
+//   }
+// });
